6826ba2183e40d7d080a08dfe0d691b026b4605b
Lily Herman <lkherman@wesleyan.edu>
2014-03-01 11:31:43 -0500

    start new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sourpatch::Application.config.secret_key_base = '5ef62ea18bcaf235f0f964c988d8b4c74fd036d59b7bb1d96d65e068ed519bb567848952b67809b19d41856b06adc8df43a07f43de118f3f75880bea927fc31a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sourpatch::Application.config.session_store :cookie_store, key: '_sourpatch_session'
